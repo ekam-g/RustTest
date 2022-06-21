27062f799797f3e1691e0a5179803a525d9b192f
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-06-21 11:05:42 -0400

    rust bad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 5;
+    let b: i32 = 25;
+    for i in 0..10 {
+        let sum = return_sum(a, b);
+        println!("The sum of a &  b is = {}", sum);
+        println!("{}",i)
+    }
+}
+
+fn return_sum(i: i32, j: i32) -> i32 {
+    i + j
 }
