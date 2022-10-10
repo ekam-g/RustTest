f8a726215ff9f2262dfc22928a84291f9663bf9c
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-10-10 00:02:34 -0400

    rust

---
+++ src/http_request/read.rs
@@ -0,0 +1,36 @@
+use reqwest::Error;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct MainParse {
+    pub error: String,
+}
+#[derive(Serialize, Deserialize)]
+pub struct Internal {}
+
+pub async fn read(url: String) -> Result<MainParse, Error> {
+    let output = reqwest::Client::new().get(url).send().await;
+    return match output {
+        Ok(data) => {
+            let final_data = data.json().await;
+            match final_data {
+                Ok(yes) => Ok(yes),
+                Err(error) => Err(error),
+            }
+        }
+        Err(err) => Err(err),
+    };
+}
+pub async fn read_more(url: String) -> Result<Internal, Error> {
+    let output = reqwest::Client::new().get(url).send().await;
+    return match output {
+        Ok(data) => {
+            let final_data = data.json().await;
+            match final_data {
+                Ok(yes) => Ok(yes),
+                Err(error) => Err(error),
+            }
+        }
+        Err(err) => Err(err),
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,41 @@
 extern crate core;
 
-use core::time;
-// use std::intrinsics::likely;
-use better_file_maker;
-use std::io::stdin;
-use std::ops::Add;
-use std::sync::mpsc;
 use std::thread;
-use std::time::Duration;
 
-fn main() {
-    //     // app_test();
-    //     // Yes().cool();
-    //     let (tx, rx) = mpsc::channel();
-    //
-    //     loop {
-    //         let x = rx.recv().unwrap();
-    //         println!("{}", x);
-    //         thread::sleep(Duration::from_millis(1000));
+mod http_request;
+
+#[tokio::main]
+async fn main() {
+    // let func = rust_store::StateData {
+    //     api_key: "your_api_key".to_string(),
+    //     // make sure to change this to some key!
+    //     // change this if your want but otherwise don't
+    //     null: "null_nil_value_key:345n,234lj52".to_string(),
+    //     // make sure this file exists or the code will not work!
+    //     data_storage_location: "database/".to_string(),
+    // };
+    // func.write_data("this is going very well", "test/worked/local", "data")
+    //     .expect("failed when writing data");
+    // func.delete_data("test/worked/local/data").expect("");
+    // func.null_write("test/worked/local/data").expect("");
+    // let error = func.read_data("test/worked/local/data");
+    // match error {
+    //     Ok(_) => {
+    //         panic!("bad")
+    //     }
+    //     Err(_) => {
+    //         func.delete_data("test/worked/local/data").expect("");
+    //         func.start_database_online();
+    //         return;
     //     }
     // }
-    //
-    // fn test(mut y: i32){
-    //     let testfunc = thread::spawn( move|| {
-    //         loop {
-    //             y = y + 1;
-    //             thread::sleep(Duration::from_millis(1000));
-    //             tx.send(y).unwrap();
-    //         }
-    //     });
-    //     Yes().add_for();
-    // fard::works();
-    Yes {}.test_crate();
-}
-
-struct Yes {}
-
-trait Cool {
-    fn cool(&self);
-}
-
-impl Cool for Yes {
-    fn cool(&self) {
-        let a = 5;
-        let b: i32 = 25;
-        for i in 0..10 {
-            let sum = return_sum(a, b);
-            println!("The sum of a &  b is = {}", sum);
-            println!("{}", i)
-        }
+    let cool = "hi I will crash your computer".to_owned();
+    let mut num: i128 = 0;
+    loop {
+        num = num + 1;
+        println!("{}", num);
+        cool.clone();
+        num.clone();
     }
 }
-
-fn return_sum(i: i32, j: i32) -> i32 {
-    i + j
-}
-
-trait App {
-    fn app_test(&self);
-}
-
-impl App for Yes {
-    fn app_test(&self) {
-        loop {
-            let mut a = String::new();
-            let mut b = String::new();
-
-            println!("write a number");
-            stdin().read_line(&mut a).unwrap();
-            if a == "exit" {
-                break;
-            }
-            let a: i32 = a.trim().parse().unwrap();
-            stdin().read_line(&mut b).unwrap();
-
-            let b: i32 = b.trim().parse().unwrap();
-            println!("{}", a + b);
-        }
-    }
-}
-
-impl Yes {
-    fn add_for(&self) {
-        let mut x: i32 = 0;
-        loop {
-            x = x + 1;
-            println!("{}", &x);
-            if x == 10000 {
-                break;
-            }
-        }
-    }
-    fn test_crate(&self) {
-        better_file_maker::make_folders("test/yes/cool/yes/cool".to_string()).expect("");
-    }
-}
-
-impl Yes {
-    fn overflow() {
-        let mut x: String = "word".to_string();
-        loop {
-            x = x.add(" word");
-            print!("{}\n", &x);
-            // thread::sleep(time::Duration::from_millis(0));
-        }
-    }
-}
+//http://10.50.33.48:8081/topic.php?t=Chronology
